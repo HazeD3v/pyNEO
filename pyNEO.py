e63d967536463beb232817ee20fcfb21cc42ef28
HazeD3v <131150853+HazeD3v@users.noreply.github.com>
2023-04-20 18:55:56 +0930

    Update pyNEO.py

--- pyNEO.py
+++ pyNEO.py
@@ -14,9 +14,7 @@ def get_neo_data(url):
         response = request.urlopen(url)
     except error.HTTPError as http_error:
         sys.exit(f"Something went wrong! ({http_error.code})")
-    # Store the data from the response
     data = response.read()
-    # Try to JSONify the result
     try:
         return json.loads(data)
     except json.JSONDecodeError:
@@ -27,21 +25,30 @@ def get_neo_data(url):
 def disply_neo_data(neo_data):
     # Amount of NEO's today
     neo_amount = neo_data["element_count"]
-
-    # Display how many NEO's there are TODAY
-    print(colored("🔭Amount of NEO'S TODAY:", "cyan", "on_black"))
-    print(colored(f"{neo_amount}\n", "light_red"))
-
     # 'neos' variable is the stage before having to iterate over each NEO 
     neos = neo_data["near_earth_objects"][f"{DATE}"]
+
+    # Store data about the NEO's
+    fast_neo = []
+    big_neo = []
+    miss_neo = []
+
+    # Loop over each NEO and append data
+    i = 0
+    while i < neo_amount:
+        big_neo.append(neos[i]["estimated_diameter"]["kilometers"]["estimated_diameter_max"])
+        miss_neo.append(neos[i]["close_approach_data"][0]["miss_distance"]["kilometers"])
+        fast_neo.append(neos[i]["close_approach_data"][0]["relative_velocity"]["kilometers_per_hour"])
+        i += 1
 
     # Store how many are potentially hazardous asteroids and their details
     hazardous_neo_amount = 0
     hazardous_neo_names = []
     hazardous_neo_diameters = []
     hazardous_neo_miss_distance = []
+    hazardous_neo_velocity = []
 
-    # Loop over each NEO and append details to the lists
+    # Loop over each NEO and append details
     x = 0
     while x < neo_amount:
         if neos[x]["is_potentially_hazardous_asteroid"] == True:
@@ -49,7 +56,12 @@ def disply_neo_data(neo_data):
             hazardous_neo_names.append(neos[x]["name"])
             hazardous_neo_diameters.append(neos[x]["estimated_diameter"]["meters"]["estimated_diameter_max"])
             hazardous_neo_miss_distance.append(neos[x]["close_approach_data"][0]["miss_distance"]["kilometers"])
+            hazardous_neo_velocity.append(neos[x]["close_approach_data"][0]["relative_velocity"]["kilometers_per_hour"])
         x += 1
+    
+    # Display how many NEO's there are TODAY
+    print(colored("🔭Amount of NEO'S TODAY:", "cyan", "on_black"))
+    print(colored(f"{neo_amount}\n", "light_red"))
 
     # Check to see if there were any that are potentially hazardous Asteroids
     if hazardous_neo_amount > 0: 
@@ -61,17 +73,22 @@ def disply_neo_data(neo_data):
         while y < hazardous_neo_amount:
             print(f"{y+1}.")
             print(f"\tName: {hazardous_neo_names[y]}")
-            print(f"\tMax Est. Diameter (meters): {hazardous_neo_diameters[y]}")
-            print(f"\tMiss Distance (kilometers): {hazardous_neo_miss_distance[y]}")
+            print(f"\tMax Est. Diameter: {hazardous_neo_diameters[y]} km")
+            print(f"\tMiss Distance: {hazardous_neo_miss_distance[y]} km")
+            print(f"\Velocity: {hazardous_neo_velocity[y]} kph\n")
             y += 1
+    # If there is no Hazardous NEO's display some stats along with the okay symbol
     else:
+        # Display NEO Stats
+        print(f"Largest Estimated Maximum Diameter: {max(big_neo)} km\n")
+        print(f"Miss Distance of the nearest NEO : {max(miss_neo)} km\n")
+        print(f"Highest Velocity: {max(fast_neo)} km/h\n")
+        # Give it the okay
         print(colored("👌No Potentially Hazardous NEO's", "green", "on_black"))
 
-    
 
-print("Displays Todays Near Earth Objects\nAlso shows a bit of data about the ones that come a little close!\n")
-
-
+# Loads before data. Gives the user something to stare at I guess
+print("Displays Todays Near Earth Objects\n")
 # Give get_neo_data() the API URL and store the results in neo_data
 neo_data = get_neo_data(API_URL)
 # Display the results from neo_data
